c99e24c3bda270540ca129d3a33a2f2f4f7bfa65
abbas-syslab <163228695+abbas-syslab@users.noreply.github.com>
2024-03-25 23:17:27 +0500

    - Added enum for user roles - Invoice interface - user interface

---
+++ src/app/interfaces/invoice.interface.ts
@@ -0,0 +1,8 @@
+export interface Invoice {
+  id: number;
+  items: {},
+  quantities: number,
+  prices: number,
+  payment_status: string,
+  payment_type: string
+}
---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+import { UserRoles } from '../enums/user-roles.enum'
+
+export interface User {
+  id: number;
+  username: string;
+  password: string;
+  role: UserRoles; // Use the enum here
+}
