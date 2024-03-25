7fe56418c21de454020701183bef23c8ccee993c
abbas-syslab <163228695+abbas-syslab@users.noreply.github.com>
2024-03-26 02:54:06 +0500

    Show Invoice list

---
+++ src/app/components/invoice/invoice-item/invoice-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserRoles } from 'src/app/enums/user-roles.enum';
+import { User } from 'src/app/interfaces/user.interface';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-invoice-item',
+  templateUrl: './invoice-item.component.html',
+  styleUrls: ['./invoice-item.component.scss']
+})
+export class InvoiceItemComponent {
+  @Input() invoice: any;
+
+  userRoles = UserRoles;
+  user!: User
+
+  constructor(private authService: AuthService, private router: Router) {
+    console.log('Invoice item:-', this.invoice)
+    this.authService.getUsers().subscribe(users => {
+      this.user = users.find((u: any) => u.id == localStorage.getItem('userId'));
+    })
+  }
+
+  editInvoice(id: string): void {
+    this.router.navigate(['/create_invoice'], { state: { id } });
+  }
+}
---
+++ src/app/components/invoice/invoice-lists/invoice-lists.component.ts
@@ -0,0 +1,38 @@
+
+import { Component } from '@angular/core';
+import { UserRoles } from 'src/app/enums/user-roles.enum';
+import { User } from '../../../interfaces/user.interface';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-invoice-lists',
+  templateUrl: './invoice-lists.component.html',
+  styleUrls: ['./invoice-lists.component.scss']
+})
+export class InvoiceListsComponent {
+
+  showForm: boolean = false
+  showList: boolean = false
+  invoices: any = []
+  user!: User
+
+  constructor(private authService: AuthService, private router: Router) {
+    localStorage.getItem('invoices')
+    if (localStorage.getItem('invoices')) {
+      let data = localStorage.getItem('invoices')
+      data ? this.invoices = JSON.parse(data) : []
+      console.log('invoices:-', this.invoices)
+      if (this.invoices.length) this.showList = true
+    }
+
+    this.authService.getUsers().subscribe(users => {
+      this.user = users.find((u: any) => u.id == localStorage.getItem('userId'));
+    })
+  }
+
+  logout() {
+    localStorage.clear()
+    location.reload()
+  }
+}
