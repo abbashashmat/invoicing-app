3020f49e0f92e4289aed1f990ec9280211c68378
abbas-syslab <163228695+abbas-syslab@users.noreply.github.com>
2024-03-25 23:54:35 +0500

    Bootstrap added and remove extra code from app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateInvoiceComponent } from './components/invoice/create-invoice/create-invoice.component';
+import { InvoiceListsComponent } from './components/invoice/invoice-lists/invoice-lists.component';
+import { EditInvoiceComponent } from './components/invoice/edit-invoice/edit-invoice.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/auth/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InvoiceItemComponent } from './components/invoice/invoice-item/invoice-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateInvoiceComponent,
+    InvoiceListsComponent,
+    EditInvoiceComponent,
+    LoginComponent,
+    InvoiceItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
