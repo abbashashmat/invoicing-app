2a754e11480bee47287c40d8bfd3b850917ac02d
abbas-syslab <163228695+abbas-syslab@users.noreply.github.com>
2024-03-26 00:53:19 +0500

    Login implemented

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  login(): void {
+    // Check if username and passowrd is not empty
+    if ((this.email || "") == "" && (this.password || "") == "") {
+      return
+    }
+
+    if ((this.email || "") != "" && (this.password || "") != "") {
+      this.authService.getUsers().subscribe(users => {
+        const user = users.find((u: any) => u.email === this.email && u.password === this.password);
+        if (user) {
+          alert('Login successful');
+          localStorage.setItem('userId', user.id)
+          // Redirect or perform other actions upon successful login
+          this.router.navigate(['/']);
+        } else {
+          alert('Invalid username or password');
+        }
+      });
+    }
+  }
+
+}
